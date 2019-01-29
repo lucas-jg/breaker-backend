8cf91a293b9dc4f9826737d31583d6c738379604
Lucas <kjk727@me.com>
2019-01-29 15:01:48 +0900

    Applying environment variables and connecting databases

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+require("dotenv").config();
+
 const Koa = require("koa");
 const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 
 // Import Module
 const api = require("./api");
+
+const mongoose = require("mongoose");
+
+const { PORT: port = 7436, MONGO_URI: mongoURI } = process.env;
+
+// Node의 Promise를 사용하도록 설정
+mongoose.Promise = global.Promise;
+mongoose
+    .connect(mongoURI)
+    .then(() => {
+        console.log("Connected to MongoDB => " + mongoURI);
+    })
+    .catch(e => {
+        console.log(e);
+    });
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +33,6 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(7436, () => {
-    console.log("listeing to port 7436");
+app.listen(port, () => {
+    console.log("listeing to port : " + port);
 });
