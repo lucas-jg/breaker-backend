b0a59a35a740b10ae0513adab9303c1f9a815179
Lucas <kjk727@me.com>
2019-01-29 15:31:47 +0900

    Schema initialization

---
+++ src/schemas/map.js
@@ -0,0 +1,34 @@
+const mongoose = require(mongoose);
+
+const { Schema } = mongoose;
+
+const MapData = new Schema({
+    blockID: Number,
+    iIndex: Number
+});
+
+const Map = new Schema({
+    title: String,
+    count: {
+        type: Number,
+        default: 0
+    },
+    owner: String,
+    bestScore: {
+        user: {
+            type: String,
+            default: "uknown"
+        },
+        score: {
+            type: Number,
+            default: 0
+        }
+    },
+    mapData: [MapData],
+    createDate: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+module.exports = mongoose.model("Map", Map);
